7c8ef0769f704ec1abcefed9cca1790fc5a8b8fd
dalveersingh <dalveerchu1@gmail.com>
2025-04-09 09:24:15 +0530

    operators and if statement

---
+++ 9. if statement.php
@@ -0,0 +1,26 @@
+<?php
+// if statement one condition is true in executes code
+
+if (22 > 20) {
+    echo "Have a good day <br>";
+}
+
+//if statement We can use variables
+$t = 14;
+if ($t < 20) {
+    echo "dalveer singh <br>";
+}
+
+//new syntax
+$a = 12;
+$b = 19;
+if ($a == $b):
+    echo "new syntax <br>";
+endif;
+echo "this program not working";
+
+
+?>
+
+
+<!-- <h1></h1> -->
