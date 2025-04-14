b5100254f674aee45b8c9706764d5a5052805d64
dalveersingh <dalveerchu1@gmail.com>
2025-04-14 11:46:09 +0530

    correction this file constant variable

--- 6. constant_variable.php
+++ 6. constant_variable.php
@@ -1,12 +1,12 @@
 <?php
 // constant variables are global variables
-// Constants do not have a dollar sign ( $ )
+// Constants do not used a dollar sign ( $ )
 // not re define and value not re assign
 
 Syntax:
 // define(name, value);
 
 define("test", 50);
-// echo test;
+// echo test
 $sum = test + 20;
 echo $sum;   //output = 70
