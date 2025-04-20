c4918c5c4d96a96e661265ed19bbc9f5a097f867
dalveersingh <dalveerchu1@gmail.com>
2025-04-20 23:15:00 +0530

    Multidimensional Arrays

---
+++ 22.2 Multidimensional Arrays.php
@@ -0,0 +1,64 @@
+<?php
+// Multidimensional Array same used Nested Array
+
+
+// Multidimensional Array
+$mulit = [
+    [1, "dalveer", "singh", 50000],
+    [2, "dharmveer", "singh", 200000]
+];
+echo $mulit[1][1] . "<br>";
+
+
+
+//  Multidimensional Array used for Loop
+$mul = [
+    [1, "dalveer", "singh", 50000],
+    [2, "dharmveer", "singh", 200000],
+    [3, "dalveer", "singh", 50000],
+    [4, "dharmveer", "singh", 200000]
+];
+for ($row = 0; $row < 4; $row++) {
+    for ($col = 0; $col < 4; $col++) {
+        echo $mul[$row][$col] . " ";
+    }
+    echo "<br>";
+}
+//  Multidimensional Array used foreach Loop
+$mul = [
+    [1, "dalveer", "singh", 50000],
+    [2, "dharmveer", "singh", 200000],
+    [3, "dalveer", "singh", 50000],
+    [4, "dharmveer", "singh", 200000]
+];
+foreach ($mul as $v1) {
+    foreach ($v1 as $v2) {
+        echo $v2 . " ";
+    }
+    echo "<br>";
+}
+
+//  Multidimensional Array used foreach Loop create Table
+$mul = [
+    [1, "dalveer", "singh", 50000],
+    [2, "dharmveer", "singh", 200000],
+    [3, "dalveer", "singh", 50000],
+    [4, "dharmveer", "singh", 200000]
+];
+echo "<table border='2px' cellpadding='2px' cellspacing='0'>";
+echo "<tr>
+   <th>Emp Id </th>
+   <th>Emp Name </th>
+   <th>Designation </th>
+   <th>Salary </th>
+</tr>";
+
+foreach ($mul as $v1) {
+    echo "<tr>";
+
+    foreach ($v1 as $v2) {
+        echo "<td>$v2 </td>";
+    }
+    echo "<tr>";
+}
+echo "</table>";
