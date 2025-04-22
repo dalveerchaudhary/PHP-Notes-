2a6d2be2562353b2b3e682ddd7928763024e7c5a
dalveersingh <dalveerchu1@gmail.com>
2025-04-22 22:20:36 +0530

    list function and count and sizeof functions

--- 22.4 List Function  Multidimensional Array.php
+++ 22.4 List Function  Multidimensional Array.php
@@ -1,5 +1,10 @@
 <?php
-// 
+// The list() function is used to assign values to a list of variables in one operation
+// List() function only worked on numerical arrays.
+
+
+Syntax:
+// list(var1, var2, ...)
 
 $emp = [
     [1, "dalveer", "developer", 50000],
@@ -11,3 +16,62 @@ $emp = [
 foreach ($emp as list($id, $name, $designation, $salary)) {
     echo "$id $name $designation $salary <br>";
 }
+
+
+// list function use to create table index Array
+$emp = [
+    [1, "dalveer", "developer", 50000],
+    [2, "dharmveer", "web developer", 200000],
+    [3, "neta", "designer", 50000],
+    [4, "anoop", "web designer", 50000]
+];
+echo "<table border='2px' cellpadding='5px' cellspacing='0px'>
+   <tr>
+   <th>Emp Id.</th>
+   <th>Name</th>
+   <th>Designation</th>
+   <th>Salary</th>
+   </tr> ";
+foreach ($emp as list($id, $name, $designation, $salary)) {
+    echo "<tr> <td>$id</td> <td>$name</td> <td>$designation</td> <td>$salary </td> <tr> <br>";
+}
+echo "</table>";
+
+// list function use to create table Associative Array
+$associative = [
+    [
+        "id" => 1,
+        "name" => "dalveer",
+        "designation" => "Manager",
+        "salary" => 50000
+    ],
+    [
+        "id" => 2,
+        "name" => "dharmveer",
+        "designation" => "designer",
+        "salary" => 15000
+    ],
+    [
+        "id" => 3,
+        "name" => "neta",
+        "designation" => "developer",
+        "salary" => 5000
+    ],
+    [
+        "id" => 3,
+        "name" => "neta",
+        "designation" => "developer",
+        "salary" => 5000
+    ]
+];
+echo "<table border='2px' cellpadding='5px' cellspacing='0px'>
+   <tr>
+   <th>Emp Id.</th>
+   <th>Name</th>
+   <th>Designation</th>
+   <th>Salary</th>
+   </tr> ";
+foreach ($associative as list("id" => $id, "name" => $name, "designation" => $designation, "salary" => $salary)) {
+    echo "<tr> <td>$id</td> <td>$name</td> <td>$designation</td> <td>$salary </td> <tr> <br>";
+}
+echo "</table>";
---
+++ 22.5 Array Count & Sizeof Function.php
@@ -0,0 +1,9 @@
+<?php
+// 
+
+Syntax:
+// sizeof(array, mode)
+
+$food = array('orange', 'banana', 'apple', 'grapes');
+// echo count($food);       // used count 
+echo sizeof($food);   // used sizeof 
