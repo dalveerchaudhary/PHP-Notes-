b39b7c93be32fa592b39d487a5b69d5d3548269c
dalveersingh <dalveerchu1@gmail.com>
2025-04-26 23:40:12 +0530

    Str_split & Chunk_split Function

---
+++ 24.1 Str_split & Chunk_split Function.php
@@ -0,0 +1,13 @@
+<?php
+// str_split()
+// The str_split() function splits a string into an array.
+
+Syntax:
+// str_split(string,length)
+
+$str = "dalveer singh";
+$array = str_split($str);
+
+echo "<pre>";
+print_r($array);
+echo "</pre>";
